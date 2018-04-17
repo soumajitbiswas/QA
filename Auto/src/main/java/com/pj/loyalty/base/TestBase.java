d5c5365ed0dffc40188c1e60552e1c87ab2fd54a
soumajitbiswas <soumajit.biswas@outlook.com>
2018-04-17 12:23:00 -0400

    Test

--- Auto/src/main/java/com/pj/loyalty/base/TestBase.java
+++ Auto/src/main/java/com/pj/loyalty/base/TestBase.java
@@ -1,5 +1,73 @@
 package com.pj.loyalty.base;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.events.EventFiringWebDriver;
+
+import com.pj.loyalty.util.TestUtil;
+import com.pj.loyalty.util.WebEventListener;
 
 public class TestBase {
+	
+	public static WebDriver driver;
+	public static Properties prop;
+	public  static EventFiringWebDriver e_driver;
+	public static WebEventListener eventListener;
+	
+	public TestBase(){
+		try {
+			prop = new Properties();
+			FileInputStream ip = new FileInputStream(System.getProperty("user.dir")+ "/src/main/java/com/pj/loyalty/config"
+					+ "config.properties");
+			prop.load(ip);
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	
+	public static void initialization(){
+		String browserName = prop.getProperty("browser");
+		
+		if(browserName.equals("chrome")){
+			System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir")
+					+"/src/main/java/com/pj/loyalty/config/chromedriver_Win32");	
+			driver = new ChromeDriver(); 
+		}
+		else if(browserName.equals("FF")){
+			System.setProperty("webdriver.gecko.driver", "/Users/naveenkhunteta/Documents/SeleniumServer/geckodriver");	
+			driver = new FirefoxDriver(); 
+		}
+		
+		
+		e_driver = new EventFiringWebDriver(driver);
+		// Now create object of EventListerHandler to register it with EventFiringWebDriver
+		eventListener = new WebEventListener();
+		e_driver.register(eventListener);
+		driver = e_driver;
+		
+		driver.manage().window().maximize();
+		driver.manage().deleteAllCookies();
+		driver.manage().timeouts().pageLoadTimeout(Long.valueOf(prop.getProperty("Page_Load_Time")), TimeUnit.SECONDS);
+		driver.manage().timeouts().implicitlyWait(Long.valueOf(prop.getProperty("Implicit_Wait")), TimeUnit.SECONDS);
+		
+		driver.get(prop.getProperty("url"));
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
 
 }
