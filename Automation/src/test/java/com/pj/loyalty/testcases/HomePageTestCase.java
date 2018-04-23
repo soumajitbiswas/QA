db85f6db4d1aa65e0fb5366a46bdc9f67bfc6b94
soumajitbiswas <soumajitbiswas@10.0.0.176>
2018-04-23 18:08:13 -0500

    Test

--- Automation/src/test/java/com/pj/loyalty/testcases/HomePageTestCase.java
+++ Automation/src/test/java/com/pj/loyalty/testcases/HomePageTestCase.java
@@ -1,7 +1,14 @@
 package com.pj.loyalty.testcases;
 
 import com.pj.loyalty.base.TestBase;
+import com.pj.loyalty.pages.HomePage;
 
 public class HomePageTestCase extends TestBase{
 
+	HomePage homePage;
+	public HomePageTestCase() {
+		super();
+	}
+	
+	
 }
