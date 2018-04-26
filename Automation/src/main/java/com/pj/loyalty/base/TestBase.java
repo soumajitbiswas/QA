e41cb4f437726dc97032878281cee93b67aa5c4f
soumajitbiswas <soumajitbiswas@10.0.0.176>
2018-04-26 14:15:53 -0500

    Test

--- Automation/src/main/java/com/pj/loyalty/base/TestBase.java
+++ Automation/src/main/java/com/pj/loyalty/base/TestBase.java
@@ -39,8 +39,7 @@ public class TestBase {
 		String browserName = prop.getProperty("browser");
 		
 		if(browserName.equals("chrome")){
-			System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir")
-					+ "/src/main/java/com/pj/loyalty/util/chromedriver_MAC64");	
+			System.setProperty("webdriver.chrome.driver", "/Users/soumajitbiswas/Downloads/Browser/chromedriver");	
 			driver = new ChromeDriver(); 
 		}
 		else if(browserName.equals("FF")){
