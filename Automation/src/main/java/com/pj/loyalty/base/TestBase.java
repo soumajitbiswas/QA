408aca35c7c53a71d0e5729de786a403b0268837
soumajitbiswas <soumajitbiswas@10.0.0.176>
2018-04-26 12:54:16 -0500

    commited

--- Automation/src/main/java/com/pj/loyalty/base/TestBase.java
+++ Automation/src/main/java/com/pj/loyalty/base/TestBase.java
@@ -8,10 +8,7 @@ import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.firefox.FirefoxBinary;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.firefox.FirefoxOptions;
-import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.support.events.EventFiringWebDriver;
 
 import com.pj.loyalty.util.WebEventListener;
